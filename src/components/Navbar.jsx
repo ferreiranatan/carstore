dcbfd9aaa86697c42a5a920fbf97d6e1da4e6329
ferreiranatan <natan_ael@hotmail.com>
2024-03-14 13:15:54 -0300

    criaçao do Navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Navbar = () =>{
+    return (
+        <nav>
+            <ul>
+                <li><a href="/">Ofertas</a></li>
+                <li><a href="/adm">Administração</a></li>
+            </ul>
+        </nav>
+    )
+}
+
+export default Navbar
